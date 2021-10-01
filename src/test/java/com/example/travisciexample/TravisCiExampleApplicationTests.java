256d5807b22b6d5109d9b6e9bc5ac6967987f38b
Sahand Saniee Monfared <ssmonfared@ce.sharif.edu>
2021-10-01 00:24:35 -0500

    [add] travis ci support

---
+++ src/test/java/com/example/travisciexample/TravisCiExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travisciexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravisCiExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
